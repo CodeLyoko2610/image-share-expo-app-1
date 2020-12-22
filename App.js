b5e60b4bf219fd3428d45754286b814dc4f0a6a9
Dang Chuong Pham <dangchuong1999@outlook.com>
2020-12-22 11:31:54 +0200

    Add a button.

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 
 export default function App() {
   return (
     <View style={styles.container}>   
         <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={styles.logo}/> 
-       <Text style={styles.instruction}>To share a photo from your phone with a friend, just press the button below!</Text>    
+       <Text style={styles.instruction}>To share a photo from your phone with a friend, just press the button below!</Text>  
+
+       <TouchableOpacity
+        onPress={() => alert('Hello World!')}
+        style={styles.button}        
+       >
+         <Text style={styles.buttonText}>Pick a photo</Text>
+       </TouchableOpacity>
     </View>
   );
 }
@@ -27,5 +34,14 @@ const styles = StyleSheet.create({
     width: 305,
     height: 159,
     marginBottom: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
   }
 });
